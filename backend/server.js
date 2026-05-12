4976ee89e8613b5147f630887b4cf760b09addf3
FriendlyUser1 <felix@ciiro.dev>
2026-05-12 14:44:10 +0000

    update binding

--- backend/server.js
+++ backend/server.js
@@ -105,6 +105,6 @@ app.post("/api/receipts", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
+app.listen(PORT, "127.0.0.1", () =>
 	console.log(`Server running on http://localhost:${PORT}`),
 );
